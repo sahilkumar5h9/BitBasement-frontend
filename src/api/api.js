bb18c2fd3253b5cdad5b199a08cf321f5a5fbaf9
cheega <wzmaust1@gmail.com>
2025-12-29 18:36:30 +0530

    dsda

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
 
 export const signup = async (data) => {
   const res = await fetch(`${BASE_URL}/auth/signup`, {
